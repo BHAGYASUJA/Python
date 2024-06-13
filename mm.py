fdcde7d70ce0c5715eac2af8e85d648add6658b3
BHAGYASUJA123 <BHAGYASUJA123>
2024-06-13 13:32:33 +0530

    mmm

---
+++ mm.py
@@ -0,0 +1,10 @@
+my_list = [67, 2, 999, 1, 15, 20, 25, 10, 72, 100]
+
+# this prints the unordered list
+print("Given Numbers: ", my_list)
+
+# sorts the list in place
+my_list.sort()
+
+# this prints the ordered list
+print("Sorted list: ", my_list)
